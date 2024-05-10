046d6fd56a6509988957499caa1266fef5bde0a6
lc2122 <76505990+lc2122@users.noreply.github.com>
2024-05-10 11:43:46 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-$(window).on('load', function () {
-    $('#m3u8-placeholder')[0].value = localStorage.getItem('m3u8-link') || '';
+$(window).on('load', function () { 
+    $('#m3u8-placeholder')[0].value = localStorage.getItem('m3u8-link') || ''; 
     $('#play-btn').on('click', function () {
-        var shortenedUrl = $('#m3u8-placeholder')[0].value; // 단축 URL로 리다이렉트할 링크를 입력받음
+        var shortenedUrl = $('#m3u8-placeholder')[0].value;
         localStorage.setItem('m3u8-link', shortenedUrl);
-        // 아래 코드에서 '#'+shortenedUrl 부분을 수정하여 단축 URL로 리다이렉트할 수 있습니다
-        window.location.href = './player' + '#' + shortenedUrl;
+        window.location.href = './player?url=' + shortenedUrl;
     });
 });
