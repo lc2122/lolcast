53b94555749c171a41632c1ff857fed761cfda79
lc2122 <76505990+lc2122@users.noreply.github.com>
2024-05-10 11:55:51 +0900

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-$(window).on('load', function () { 
-    $('#m3u8-placeholder')[0].value = localStorage.getItem('m3u8-link') || ''; 
-    $('#play-btn').on('click', function () {
-        var shortenedUrl = $('#m3u8-placeholder')[0].value;
-        localStorage.setItem('m3u8-link', shortenedUrl);
-        window.location.href = './player?url=' + shortenedUrl;
-    });
-});
