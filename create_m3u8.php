df569a8cc5205b4430233b2bac28ce71d0f8105b
lc2122 <76505990+lc2122@users.noreply.github.com>
2024-05-10 15:31:55 +0900

    Update create_m3u8.php

--- create_m3u8.php
+++ create_m3u8.php
@@ -2,4 +2,8 @@
 if (isset($_GET['url'])) {
     $url = $_GET['url'];
     $m3u8_content = "#EXTM3U\n#EXT-X-VERSION:3\n#EXT-X-STREAM-INF:BANDWIDTH=800000\n{$url}";
-    $m3u8_filename = uniqid() . '.m3u
+    $m3u8_filename = uniqid() . '.m3u8';
+    file_put_contents($m3u8_filename, $m3u8_content);
+    echo $m3u8_filename;
+}
+?>
