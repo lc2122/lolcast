492e65b5da3ec1bdfd16abbb1284e2ee097bec3d
lc2122 <76505990+lc2122@users.noreply.github.com>
2024-05-10 15:38:37 +0900

    Delete create_m3u8.php

--- create_m3u8.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_GET['url'])) {
-    $url = $_GET['url'];
-    $m3u8_content = "#EXTM3U\n#EXT-X-VERSION:3\n#EXT-X-STREAM-INF:BANDWIDTH=800000\n{$url}";
-    $m3u8_filename = uniqid() . '.m3u8';
-    file_put_contents($m3u8_filename, $m3u8_content);
-    echo $m3u8_filename;
-}
-?>
