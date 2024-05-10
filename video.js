5a9e794146750249733db6f9ae237609b32195c6
lc2122 <76505990+lc2122@users.noreply.github.com>
2024-05-10 11:15:41 +0900

    Update player.js

--- video.js
+++ video.js
@@ -1,2 +1,20 @@
-<input type="text" id="streamUrl" placeholder="Enter HLS Streaming URL"> 
-  <button onclick="loadStream()">Load Stream</button> <script> function loadStream() { var videoUrl = document.getElementById("streamUrl").value; if (videoUrl) { var video = document.getElementById('videoPlayer'); if (Hls.isSupported()) { var hls = new Hls(); hls.loadSource(videoUrl); hls.attachMedia(video); hls.on(Hls.Events.MANIFEST_PARSED,function() { video.play(); }); } else if (video.canPlayType('application/vnd.apple.mpegurl')) { video.src = videoUrl; video.addEventListener('loadedmetadata',function() { video.play(); }); } } else { alert("Please enter a valid HLS streaming URL"); } } </script>
+const videoPlayer = document.getElementById('videoPlayer');
+const inputUrl = document.getElementById('inputUrl');
+
+function playStream() {
+    const url = inputUrl.value;
+
+    if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(url);
+        hls.attachMedia(videoPlayer);
+        hls.on(Hls.Events.MANIFEST_PARSED, function() {
+            videoPlayer.play();
+        });
+    } else if (videoPlayer.canPlayType('application/vnd.apple.mpegurl')) {
+        videoPlayer.src = url;
+        videoPlayer.addEventListener('loadedmetadata', function() {
+            videoPlayer.play();
+        });
+    }
+}
