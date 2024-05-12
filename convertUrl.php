25795129d066620c8149ccdbaca69a6c0526e482
lc2122 <76505990+lc2122@users.noreply.github.com>
2024-05-13 00:07:15 +0900

    Update convertUrl.php

--- convertUrl.php
+++ convertUrl.php
@@ -1,10 +1,1 @@
-<?php
-// 단축 URL을 받아서 원래 URL로 변환하는 로직을 구현합니다.
-$shortUrl = $_GET['url'];
-
-// 예시로 is.gd의 API를 사용한 변환 방법
-$apiUrl = "https://is.gd/forward.php?format=simple&shorturl=" . $shortUrl;
-$originalUrl = file_get_contents($apiUrl);
-
-echo $originalUrl;
-?>
+<?php // 단축 URL을 받아서 원래 URL로 변환하는 로직을 구현합니다. $shortUrl = $_GET['url']; // 새로운 API를 사용하여 단축 URL을 원래 URL로 변환하는 방법 $apiUrl = "https://api.shrtco.de/v2/shorten?url=" . $shortUrl; $response = file_get_contents($apiUrl); $result = json_decode($response); if ($result->ok) { $originalUrl = $result->result->full_short_link; echo $originalUrl; } else { echo "Error occurred: " . $result->error; } ?>
