8d284d5f01c33cfecab8bf5e6b686f9300a93c16
lc2122 <76505990+lc2122@users.noreply.github.com>
2025-02-09 13:39:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,6 @@ const urlInput = document.getElementById('url-input');
 const multiviewOptions = document.getElementById('multiview-options');
 const multiviewLayoutSelect = document.getElementById('multiview-layout-select');
 const multiviewUrlInputs = document.getElementById('multiview-url-inputs');
-const addMultiviewUrlBtn = document.getElementById('add-multiview-url-btn');
 
 // 멀티뷰 관련 상태 관리
 let currentMultiviewLayout = 1;
